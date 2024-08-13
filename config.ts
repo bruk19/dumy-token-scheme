bd6892ef09329606fa3917d9737778b8b49c2589
Bruk Teshome <brukteshome21@gmail.com>
2024-08-13 16:40:39 +0300

    feat: add contract address, subgraphUrl, and reward token address

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const subgraphUrl: string = 'https://subgraph-api.mantle.xyz/subgraphs/name/diffusionlabs-frontend-hiring'
+export const contractAddress: string = "0x748ffE1e39F4CC449239Dca89Ac5949558b746a0";
+export const usdtAddress = '0x9880119542FB9e7F3464D4B086E18677652cB2dC'
+export const rewardTokenAddress = '0x6ADA3896E5Fa319a8873aAfDEf119F0e2BFCd9E0'
